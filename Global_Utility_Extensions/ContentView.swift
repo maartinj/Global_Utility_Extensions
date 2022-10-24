0ffa3e12fd2103c034b3c3c827a4d85d6e9a9a90
Marcin Jędrzejak <53284922+maartinj@users.noreply.github.com>
2022-10-24 16:22:49 +0200

    Initial Commit

---
+++ Global_Utility_Extensions/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Global_Utility_Extensions
+//
+//  Created by Marcin Jędrzejak on 24/10/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Global_Utility_Extensions/Global_Utility_ExtensionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Global_Utility_ExtensionsApp.swift
+//  Global_Utility_Extensions
+//
+//  Created by Marcin Jędrzejak on 24/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct Global_Utility_ExtensionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
